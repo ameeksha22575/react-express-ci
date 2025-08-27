6fb78c4baff34500a859d2d3b25b0b65423482e4
Ameeksha <ameeksha.jakar_cs23@gla.ac.in>
2025-08-27 19:22:24 +0530

    fix: cleaned up duplicate express code

--- server/index.js
+++ server/index.js
@@ -2,14 +2,16 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.use(cors()); 
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello from Express backend 🚀");
 });
 
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
